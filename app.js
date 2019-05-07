6466c89b3e6a7a8507a98b2dd8d5eac41c06a390
Eliasdb <elias.db3@gmail.com>
2019-05-07 09:45:13 +0200

    files

---
+++ app.js
@@ -0,0 +1,11 @@
+/* becode/javascript
+ *
+ *
+ * coded by kevin@BeCode
+ * started at 7/05/2019
+ */
+//Simple startup code
+const searchUser = document.getElementById('searchUser');
+searchUser.addEventListener('keyup', (e)=>{
+  //get the value of the input text and have fun
+})
