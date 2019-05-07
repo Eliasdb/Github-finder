9b68573a823814b5dbdcb2209563cdbe74fff11d
supriyakushavar <supriya.kushavar@gmail.com>
2019-05-07 12:07:26 +0200

    adding some code

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-/* becode/javascript
- *
- *
- * coded by kevin@BeCode
- * started at 7/05/2019
- */
-//Simple startup code
+const client_ID = 'b461dc91e5897eba844a';
+const client_Secrect = '0af9a092b4cf525a9ce6ffc646134f9fd5466894';
+
+
+
 const searchUser = document.getElementById('searchUser');
 searchUser.addEventListener('keyup', (e)=>{
-  //get the value of the input text and have fun
+  if(e.keyCode == 13){
+    user = searchUser.value;
+    async function getUserAsync(user) 
+  {
+    let response = await fetch(`https://api.github.com/users/${user}`);
+    let data = await response.json()
+    return data;
+  }
+  
+  getUserAsync(user)
+    .then(data => console.log(data)); 
+  }
+   
 })
